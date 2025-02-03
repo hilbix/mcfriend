ce21f46714b3877c6875fc127e9f30f59fdffa9e
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-03 04:09:00 +0100

    cmd/Attack.js modified

--- cmd/Attack.js
+++ cmd/Attack.js
@@ -2,6 +2,8 @@
 // (currently has no parameters)
 
 this.last ??= void 0;
+this.wait ??= 0;
+this.backoff ??= 0;
 
 function* find(_) { const e = yield ['nearest', {type:'hostile'}, _]; if (e.id) return e }
 
@@ -10,15 +12,34 @@ const e = void 0
   ?? (yield* find({name:'pillager'}))
   ?? (yield* find({name:'creeper'}))
 
-if (!e) return 'no enemy found';
+if (!e)
+  {
+    const t = yield ['set auto:attack:again'];
+    if (t !== '')
+      {
+        const b = yield ['set auto:attack:backoff'];
+        yield yield ['in', (t|0)+backoff, 'attack'];
+        backoff += b|0;
+      }
+    return ['act no enemy found'];
+  }
 if (!e.hostile) return ['act WTF? not hostile', e.id, e];
+backoff = 0;
 
 if (!(yield ['hand']).weapon)
   {
     const w = Array.from(yield ['invs']).filter(_ => _.weapon).sort((a,b) => a.dur < b.dur);
-    if (!w.length) return 'I have no weapon';
+    if (!w.length)
+      {
+        yield 'I have no weapon';
+	if (++wait>5)
+	  return 'failed to fetch weapon';
+	yield yield ['Weapon', 'sword'];
+	return ['Attack', _]
+      }
     yield yield ['equip', 'hand', w[0]];
   }
+wait = 0;
 
 const p = yield [`locate`, e];
 if ((yield ['dist', e]) >= 2)
