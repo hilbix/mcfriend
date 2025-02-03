b2aae4c52a0cbc4e9a055882e30d6116ce3eade9
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-03 04:09:07 +0100

    cmd/torch.js modified

--- cmd/torch.js
+++ cmd/torch.js
@@ -10,11 +10,21 @@ for (const a of b)
     if (a.id  !== 'air') continue;
 
     const g = yield ['block', a.pos(0,-1,0)];
-    if (g.id === 'air' || g.id === 'torch') continue;
+    switch (g.id)
+      {
+      case 'air':
+      case 'torch':
+      case 'water': continue;
+      }
 //    console.error(a, g);
 
     yield ['equip', 'hand', w[0]];
-    return ['place', w[0], a];
+    try {
+      yield ['place', w[0], a];
+    } catch (e) {
+      return 'placing torch failed';
+    }
+    return 'placed torch';
   }
 
 return 'no space to place torch';
