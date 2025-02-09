080c9fa45fdd76ffcac167d9fe4be177513b3337
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-09 22:10:02 +0100

    cmd/PUT.js modified

--- cmd/PUT.js
+++ cmd/PUT.js
@@ -1,40 +1,50 @@
 //	fill chests marked 'put' with inventory items
 
-this.lastc = void 0;
-
 function* put(item, ...where)
 {
   // this is not correct in case the put chest is full we shall fallback to store
   for (const c of (yield ['CHEST', where]) || [])
     {
       if (!c) continue;
+      if (yield ['CACHE get in', c]) continue;
 
       const h = yield ['have', item];
-      if (!h) return true;
       yield ['act have', h, item];
+      if (!h) return true;
 
-      if (lastc !== c)
-        yield yield ['Move', c, 2];
-      lastc = c;
-
-      const r = yield ['open', c];
+      const r = yield ['OPEN', c];
+      if (!r) continue;
       try {
         yield yield ['put', r, item, h];
         yield ['act putted', h, item];
       } catch (e) {
-        yield ['act OOPS', e, c];
+        if (e.message === 'destination full')
+          yield yield ['CACHE set in', c];
+        else
+          yield ['act OOPS', e.message, c];
       } finally {
-        yield ['close', r];
+        yield ['wait', 5];
       }
       if (!(yield ['have', item])) return true;
     }
 }
 
+const had = {};
+
 for (const i of yield ['invs'])
   if (i.id)
     {
-      if ((yield* put(i, 'store', i.id)) || (yield* put(i, 'put', i.id)) || (yield* put(i, 'overflow', i.id)) || (yield* put(i, 'put', 'MISC')))	// not: 'put MISC'!
-        continue;
-      await sleep(3000);
+      if (had[i.id]) continue;
+      had[i.id] = 1;
+      if ((yield* put(i, 'store', i.id)) || (yield* put(i, 'put', i.id)) || (yield* put(i, 'overflow', i.id)))
+        await sleep(3000);
     }
+
+yield ['wait'];
+
+for (const i of yield ['invs'])
+  if (i.id)
+    yield* put(i, 'put', 'MISC');
+
+yield ['OPEN'];	// close everything
 
