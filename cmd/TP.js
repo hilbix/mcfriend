c8116e79c41cc50e943b79b3ab455194dc9c9594
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-11 00:09:31 +0100

    cmd/TP.js modified

--- cmd/TP.js
+++ cmd/TP.js
@@ -1,14 +1,18 @@
 // Teleport somewhere and wait until you arrive
 
-const p = yield ['locate', _];
-//yield ['act tp', _, p];
-yield yield ['tp', p];
-for (let i = 100; --i>=0; await sleep(10))
-  {
-    const d = yield ['dist', p];
-    if (d<4)
-      return;
-  }
-
-yield ['act teleport to', p, 'failed', yield ['pos']];
-
+try {
+  const p = yield ['locate', _];
+  //yield ['act tp', _, p];
+//  yield yield ['say /gamemode spectator'];
+  yield yield ['tp', p];
+  for (let i = 100; --i>=0; await sleep(10))
+    {
+      const d = yield ['dist', p];
+      if (d<4)
+        return;
+    }
+  
+  yield ['act teleport to', p, 'failed', yield ['pos']];
+} finally {
+//  yield yield ['say /gamemode survival'];
+}
