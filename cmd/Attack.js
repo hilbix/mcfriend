fe2cc52e21940cbd8683168464a688052e23360e
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-11 00:09:30 +0100

    cmd/Attack.js modified

--- cmd/Attack.js
+++ cmd/Attack.js
@@ -8,13 +8,14 @@ this.backoff ??= 0;
 function* hostile(_) { const e = yield ['nearest', _]; if (e.id) return e }
 function* find(...a) { for (const name of a) { const e = yield* hostile(name === '.' ? [] : {name}); if (e !== void 0) return e } }
 
-const e = yield* find(...(_.length ? _ : ['phantom','witch','pillager','creeper']));
+const e = yield* find(...(_.length ? _ : ['phantom','witch','pillager','creeper','slime']));
 
 if (!e)
   {
     yield yield ['supply'];
     if (_.length)
       return ['act no enemy found', _];
+
     const t = yield ['set auto:attack:again'];
     const w = (t|0)+backoff;
     if (t !== '')
@@ -22,6 +23,8 @@ if (!e)
         yield yield ['in', w, 'attack'];
         backoff += (yield ['set auto:attack:backoff'])|0;
       }
+
+    yield yield ['home'];
     return ['act no enemy found', w/1000, _];
   }
 if (!e.hostile) return ['act WTF? not hostile', e.id, e];
