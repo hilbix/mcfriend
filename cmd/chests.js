226881da68297b418675824b6120843e3c489329
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-11 00:09:31 +0100

    cmd/chests.js added

---
+++ cmd/chests.js
@@ -0,0 +1,17 @@
+// locate signs near chests
+
+const signs = yield ['sign', _];
+if (!signs) return;
+
+for (const s of signs)
+  {
+//    if (!s.valid) continue;
+    yield ['act sign', s, yield ['locate', s]];
+
+    const d = (yield ['block', s, 6]).filter(_ => _.container);
+    if (d.length === 1)
+      yield ['act sign', s, 'at', yield ['locate', s], s.valid];
+    else
+      yield ['act invalid sign', s, 'at', yield ['locate', s], s.valid];
+  }
+
