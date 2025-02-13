58d1fff469b43c361f7b00dcaf6c51bc6d107e13
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-13 05:09:34 +0100

    cmd/PUT.js modified

--- cmd/PUT.js
+++ cmd/PUT.js
@@ -1,7 +1,9 @@
 //	fill chests marked 'put' with inventory items
 
-let hadsign;
 
+const keep = {};
+
+let hadsign;
 function* put(item, ...where)
 {
   // this is not correct in case the put chest is full we shall fallback to store
@@ -11,9 +13,9 @@ function* put(item, ...where)
       hadsign = true;
       if (yield ['CACHE get in', c]) continue;
 
-      const h = yield ['have', item];
-      yield ['act have', h, item];
-      if (!h) return true;
+      const h = (yield ['have', item]) - (keep[item.id]|0);
+//      yield ['act have', h, item];
+      if (h <= 0) return true;
 
       const r = yield ['OPEN', c];
       if (!r) continue;
@@ -32,24 +34,39 @@ function* put(item, ...where)
     }
 }
 
-const had = {};
+function* valid(i)
+{
+  const id	= i.id;
+  if (!id)		return;		// nothing is not valid
+  const _	= keep[id];
+  if (_ !== void 0)	return;		// ignore already seen
+  keep[id]	= 1;
+  const k	= keep[id]	= (yield [`set item:${i.id}:keep`])|0;
+  if (!k)		return true;
+  const h	= (yield ['have', i.id])|0;
+  return h > k;
+}
+
+const more = [], over = [];
 
 for (const i of yield ['invs'])
-  if (i.id)
+  if (yield* valid(i))
     {
-      if (had[i.id]) continue;
-      had[i.id] = 1;
-
       hadsign = false;
       if ((yield* put(i, 'store', i.id)) || (yield* put(i, 'put', i.id)) || (yield* put(i, 'overflow', i.id)) || (yield* put(i, 'toomuch', i.id)) || (hadsign && (yield* put(i, 'toomuch', 'MISC'))))
-        yield ['wait',5];
+        yield ['wait', 5];
+      else if (hadsign)
+        over.push(i);
+      else
+        more.push(i);
     }
 
-yield ['wait'];
+yield ['wait', 5];
 
-for (const i of yield ['invs'])
-  if (i.id)
-    (yield* put(i, 'put', 'MISC')) || (yield* put(i, 'overflow', 'MISC'));
+for (const i of more)
+  (yield* put(i, 'put', 'MISC')) || over.push(i);
+for (const i of over)
+  yield* put(i, 'overflow', 'MISC');
 
 yield ['OPEN'];	// close everything
 
