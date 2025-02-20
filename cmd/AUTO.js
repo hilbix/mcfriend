31090e7056def0c6e9003ec5eb1577dfea7edac8
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-20 11:37:29 +0100

    cmd/AUTO.js modified

--- cmd/AUTO.js
+++ cmd/AUTO.js
@@ -5,19 +5,21 @@
 await sleep(1);
 
 yield ['act AUTOSTART', _];
+yield ['hide Error: destination full'];
 
 yield yield ['stop'];
 yield yield ['home'];
 yield yield ['drop'];
 
-
 // This is wrong.
 //
 // The right thing is a list of creatures to automatically attack with priority.
 // Then register the list and trigger autoattack in case such a creature shows up.
+//for (const a of 
 yield ['in auto:attack:enabled attack'];
 yield ['in auto:take:enabled take'];
 yield ['in auto:tree:enabled tree'];
+yield ['in auto:harvest:enabled harvest'];
 
 return 'AUTO ended';
 
