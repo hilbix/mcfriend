32f7f47c32480ba36fcee483bc55afc8a0472925
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-21 08:26:02 +0100

    cmd/to.js modified

--- cmd/to.js
+++ cmd/to.js
@@ -1,12 +1,15 @@
 //
 
-for (const a of ['store', 'get', 'overflow', 'toomuch', 'destroy'])
+for (const a of ['store', 'get', 'overflow', 'toomuch', 'destroy', 'note'])
   {
     const s	= yield ['sign', a, _];
     if (!s?.length) continue;
 
-    yield yield ['act to', s.map(_ => `${_}`)];
-    return ['say /tp', src._, (yield ['locate', s[0]]).id];
+    if (s.length === 1)
+      {
+        yield yield ['act to', s.map(_ => `${_}`)];
+        return ['say /tp', src._, (yield ['locate', s[0]]).id];
+      }
   }
 
 return ['act not found', _];
