466c86bbb45d97dbc05129fa8468a064c60067dd
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-21 03:10:17 +0100

    cmd/tool.js modified

--- cmd/tool.js
+++ cmd/tool.js
@@ -12,7 +12,7 @@ for (let retry=2;; retry--)
   {
     const w = Array.from(yield ['invs']).filter(isTool);
     if (w.length)
-      return ['equip hand', w[0]];	// Equip tool already in inventry
+      return yield ['equip hand', w[0]];	// Equip tool already in inventory
     if (wtf<0)
       break;
     yield ['supply', `stone_${tool}`];
