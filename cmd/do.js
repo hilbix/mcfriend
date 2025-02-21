bf884bda5b3ed53ca09cf095344bf19ceccf93f0
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-02-21 02:29:54 +0100

    cmd/do.js modified

--- cmd/do.js
+++ cmd/do.js
@@ -4,5 +4,5 @@ this.hist ??= [];
 const cmd =_.map(_ => 'string' === typeof _ && _.length === 2 && _[0]==='$' ? hist[_[1]|0] : _);
 hist.unshift([yield cmd, cmd]);
 if (hist.length>10) hist.pop();
-return [ 'act result:', toJ(hist[0]) ];
+return [ 'act result:', toJ(hist[0][0]), 'cmd:', toJ(hist[0][1]) ];
 
