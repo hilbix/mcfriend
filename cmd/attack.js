473ad9d615d2445ff62ee74655d7250323ac919b
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-03-04 18:42:09 +0100

    cmd/attack.js modified

--- cmd/attack.js
+++ cmd/attack.js
@@ -1,16 +1,16 @@
 //
 
-yield ['act ATTACK start'];
+//yield ['act ATTACK start'];
 
 yield yield ['PUT'];
 
-for (;;)
+for (let cnt=0;; cnt++)
   {
     yield yield ['drop'];
     yield yield ['supply'];
     const x = yield ['Attack', _];
     if (x !== void 0)
-      return ['act ATTACK done', x];
+      return ['act ATTACK done', cnt, x];
     yield ['wait'];
     yield yield ['torch'];
     yield ['wait',2];
