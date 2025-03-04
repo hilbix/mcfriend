4a286532772c72ffec8fc7af3140b6b0ad423d6a
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-03-04 18:42:09 +0100

    cmd/to.js modified

--- cmd/to.js
+++ cmd/to.js
@@ -1,16 +1,38 @@
+// Teleport player to the given sign
 //
+// returns babble (ignore if not wanted)
 
-for (const a of ['store', 'get', 'overflow', 'toomuch', 'destroy', 'note'])
-  {
-    const s	= yield ['sign', a, _];
-    if (!s?.length) continue;
+this.last	??= {};
 
-    if (s.length === 1)
-      {
-        yield yield ['act to', s.map(_ => `${_}`)];
-        return ['say /tp', src._, (yield ['locate', s[0]]).id];
-      }
-  }
+const have = [];
 
-return ['act not found', _];
+return (yield* check(_)) || (yield* check(['store', 'get', 'overflow', 'toomuch', 'destroy', 'note'], _)) || (yield* sel()) || ['act not found', _];
+
+function* sel()
+{
+  // TODO create selector of options
+  // for now just jump to the first thing found
+  if (have.length)
+    return yield* jump(have[0][0]);
+}
+
+function* check(t, ..._)
+{
+  for (const a of t)
+    {
+      const s	= yield ['sign', a, ..._];
+      if (!s?.length) continue;
+
+      if (s.length === 1)
+        return yield* jump(s[0]);
+
+      have.push(s);
+    }
+}
+
+function* jump(_)
+{
+  yield yield ['say /tp', src._, (yield ['locate', _]).id];
+  return ['act to', _];
+}
 
