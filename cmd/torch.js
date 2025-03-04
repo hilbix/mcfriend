fc8f3b9c933583f52b0c0fe06fc57b0195469473
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-03-04 18:42:09 +0100

    cmd/torch.js modified

--- cmd/torch.js
+++ cmd/torch.js
@@ -1,17 +1,29 @@
 // place torches where it is dark
 
+this.torched	??= [];
+this.torches	??= 0;
+
+switch (_[0])
+  {
+  case 'list':
+    yield ['act', torches, 'torches', torched];
+  }
+
 const w = Array.from(yield ['invs']).filter(_ => _.id === 'torch');
 if (!w.length) return 'no torch';
 
 const p = yield ['pos'];
 const b = yield ['block', p, 27];
+const pos = [];
 for (const a of b)
   {
     if (a.id === 'torch') return;
     if (a.id !== 'air') continue;
+    if (a._.light > 10) return;
 
     // check block under torch is something we can place a torch
     const g = yield ['block', a.pos(0,-1,0)];
+    if (g.light > 10) return;
     switch (g.id)
       {
       case 'torch': return;
@@ -20,15 +32,23 @@ for (const a of b)
       }
 //    console.error(a, g);
 
-    yield ['equip', 'hand', w[0]];
-    try {
-      yield ['place', a];
-    } catch (e) {
-      return //'placing torch failed';
-    }
-    return //'placed torch';
+    pos.push(a);
   }
 
-//return 'no space to place torch';
+if (!pos.length)
+  return; //'no space to place torch';
 
+
+this.torches++;
+this.torched.push(pos[0]._.position);
+if (torched.length>100)
+  torched.shift();
+
+yield ['equip', 'hand', w[0]];
+try {
+  yield ['place', pos[0]];
+} catch (e) {
+  return //'placing torch failed';
+}
+return //'placed torch';
 
