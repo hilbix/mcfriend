a4e3fa3eb6368e3c93aece20a6328e7cfadaa437
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-03-12 18:03:03 +0100

    cmd/CraftWith.js added

---
+++ cmd/CraftWith.js
@@ -0,0 +1,66 @@
+// item count
+
+let table;
+
+for (const t of (yield ['sign', 'craft']).filter(_ => !_.text[3]))
+  {
+    const d	= (yield ['block', t, 6]).filter(_ => _.id === 'crafting_table');
+    if (d.length !== 1)
+      {
+        yield ['act #crafting? ', s];
+	continue;
+      }
+    table	= d[0];
+    break;
+  }
+
+if (!table)
+  throw 'missing sign: craft';
+
+const craftable =
+  { slime_ball:		['slime_block']
+  , sugar_cane:		['sugar', 'paper']
+  , bone_meal:		['bone_block']
+  , bamboo:		['bamboo_block']
+  , bamboo_block:	['bamboo_planks']
+  };
+
+const m = _[0];
+const n = _[1]|0;
+
+const t = craftable[m];
+if (!t)
+  throw `do not know what to craft with ${m}`;
+
+let had = 0;
+
+for (const x of t)
+  {
+    const i = yield ['item', x];
+    if (i.length !== 1)
+      throw `internal error ${x}`;
+    const i0 = i[0];
+
+    const r = (yield ['recipe', table, i0, 1]).filter(_ => _.input.length === 1 && _.input[0].id === m);
+    if (!r.length)
+      {
+        yield ['act no recipe for', i0, 'from', m];
+	continue;
+      }
+    for (const y of r)
+      {
+        const a = y.input[0].count;
+        const l = ((n/a)|0) * y.output[0].count;
+	if (!l) continue;
+
+	yield ['act', table, x, l];
+
+        yield yield ['Move', table];
+        yield yield ['craft', table, y, l];
+	yield ['wait', 20];
+	had++;
+      }
+  }
+
+return had;
+
