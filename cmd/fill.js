a97f9ee88918e5c47ec5fe0eb1e9626bf45c3b0b
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-03-12 18:03:03 +0100

    cmd/fill.js added

---
+++ cmd/fill.js
@@ -0,0 +1,71 @@
+//	actively fill chests marked 'fill' with the given items
+// This takes out the items from other chests and puts them into the given chest until it is full.
+
+this.track ??= {};
+
+yield ['act FILL start'];
+
+yield ['CACHE clear'];
+yield ['PUT'];
+
+//const a = yield ['chest', 'D'];
+const a = yield ['CHEST', 'fill'];
+if (!a?.length) return yield 'no signs with "fill"?';
+
+for (const [c,s] of a)
+  {
+    if (c.container !== true) continue;	// cannot use slotted containers
+
+    yield yield ['Move', s, 1];
+    const r	= yield ['OPEN', c];
+    if (!r) continue;
+
+    const i	= r.items().filter(yield* itemFilter(s.text[3]));
+
+    const filt	= yield* itemFilter(s.text[3]);
+    let e = 0;
+    const need = {};
+    for (const x of r.items())
+      {
+        if (x.empty)
+	  {
+	    e++;
+	    continue;
+	  }
+	if (!filt(x)) continue;
+	need[x.id] = (need[x.id]|0) + x.max - x.n;
+      }
+    const item = {};
+    for (const x of yield ['item', s.text[3]])
+      {
+        item[x.id] = x;
+	const n = need[x.id]|0;
+        need[x.id] = e ? Math.min(e,5) * x.max : n>x.max ? x.max : n <= x.max/4 ? 0 : n;
+      }
+    const m = Object.entries(need).filter(([k,v]) => v);
+    if (!m.length) continue;	// all full
+
+    try {
+      for (const [k,v] of m)
+        {
+          yield ['AGAIN fill 0'];	// reset backoff
+	  yield ['act GET',k,v];
+          yield yield [`get ${k}=${v}`];
+        }
+       yield yield ['Move', s, 1];
+       const r = yield ['OPEN', c];
+       for (const [k,v] of m)
+         {
+	   yield ['act FILL', item[k], v];
+	   yield yield ['put', r, item[k], v];
+	 }
+    } catch (e) {
+      console.error('FILL', e);
+      yield yield ['act ERROR', e];
+    }
+    yield yield ['OPEN'];		// close r
+    yield yield ['wait', 10];
+  }
+
+return ['act FILL done', yield ['AGAIN fill']];
+
