ab940259636baeb2a5090a2f9bc9913190243ab5
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-03-15 21:32:29 +0100

    cmd/attack.js modified

--- cmd/attack.js
+++ cmd/attack.js
@@ -8,6 +8,8 @@ for (let cnt=0;; cnt++)
   {
     yield yield ['drop'];
     yield yield ['supply'];
+    if (!yield ['have *sword'])
+      yield yield ['CRAFT stone_sword'];
     const x = yield ['Attack', _];
     if (x !== void 0)
       return ['act ATTACK done', cnt, x];
