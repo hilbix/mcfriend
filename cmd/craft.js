ee0f96375415817454d8b859d3bbde90f14cb1c6
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-03-15 14:43:58 +0100

    cmd/craft.js modified

--- cmd/craft.js
+++ cmd/craft.js
@@ -1,9 +1,12 @@
 //
 
 try {
-  if (yield ['CRAFT', _]) return;
-} catch {
+  const r = yield ['CRAFT', _];
+  if (r) return ['act craft ok', r, _];
+} catch(e) {
+  return ['act craft fail', e];
 }
 if (!_.length)
   yield ['in 50 craft'];
+return ['act craft end', _];
 
