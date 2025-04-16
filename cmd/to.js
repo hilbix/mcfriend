97d44881c251ce69d2e118914e798672585ba3d7
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-04-16 19:53:59 +0200

    cmd/to.js modified

--- cmd/to.js
+++ cmd/to.js
@@ -4,21 +4,25 @@
 
 this.hist	??= [];
 
-const pos	= yield ['pos', src];
+//const pos	= yield ['pos', src];
 //hist		= hist.filter(_ => pos.
 
 const have = [];
 
 //if (!_.length)
 
-return (yield* check(_)) || (yield* check(['note', 'store', 'get', 'overflow', 'toomuch', 'destroy'], _)) || (yield* sel()) || ['act not found', _];
+return (yield* check(_)) || (yield* check(['note', 'get', 'store', 'overflow', 'toomuch', 'destroy'], _)) || (yield* sel()) || ['act not found', _];
 
 function* sel()
 {
   // TODO create selector of options
   // for now just jump to the first thing found
-  if (have.length)
-    return yield* jump(have[0][0]);
+  console.log('HAVE', have);
+  while (have.length)
+    {
+      const s = have.shift();
+      return yield* jump(s[0]);
+    }
 }
 
 function* check(t, ..._)
@@ -28,9 +32,9 @@ function* check(t, ..._)
       const s	= yield ['sign', a, ..._];
       if (!s?.length) continue;
 
-      if (s.length === 1)
-        return yield* jump(s[0]);
+//      if (s.length === 1) return yield* jump(s[0]);
 
+      console.log('CHECK', s);
       have.push(s);
     }
 }
