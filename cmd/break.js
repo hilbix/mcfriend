726ab9b9e70da3af6501f011c668582bceaa18af
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-04-21 19:31:56 +0200

    cmd/break.js added

---
+++ cmd/break.js
@@ -0,0 +1,19 @@
+// break needs 2 signs which define the area
+//
+// within the AREA the given blocks are broken
+// Use the full area.
+//
+// Note:  This might break blocks the sign is on!
+
+const keep = {lava:true, water:true};
+const nix = { air:1, cobblestone_slab:1, cave_air:1, torch:1, wall_torch:1 };
+
+const b = yield ['AREA break'];
+if (!b) return 'act no area yet';
+
+for (const a of b)
+  {
+    const iter = yield ['block', a[0], a[1]];
+    yield yield ['DIG', iter, a[0].text[3]];
+  }
+
