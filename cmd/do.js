3c1822224421d67d5d4938ae4da19cc23c5712d0
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-05-05 03:55:26 +0200

    cmd/do.js modified

--- cmd/do.js
+++ cmd/do.js
@@ -1,8 +1,42 @@
 
 this.hist ??= [];
 
-const cmd =_.map(_ => 'string' === typeof _ && _.length === 2 && _[0]==='$' ? hist[_[1]|0][0] : _);
-hist.unshift([yield cmd, cmd]);
+const cmd =_.map(_ => 'string' !== typeof _ || _[0]!=='$' ? _ : Array.from(_.substr(2)).reduce((a,_) => a[_], hist[_[1]|0][0]));
+const r = yield cmd;
+hist.unshift([r, cmd]);
 if (hist.length>10) hist.pop();
-return [ 'act result:', toJ(hist[0][0]), 'cmd:', toJ(hist[0][1]) ];
+
+//console.error('res', r);
+
+return [ 'act result', yield* dump('res', r), 'cmd:', (yield* dumper([], cmd)).map(_ => _[1])];
+
+function* dump(...a)
+{
+  const n = yield* dumper(...a);
+  for (const i in n)
+    yield ['act', i, n[i].join(': ')];
+  return n.length;
+}
+
+function* dumper(p, _, n=[])
+{
+  if (Array.isArray(_))			for (const i in _) yield* dumper(`${p}[${i}]`, _[i], n);
+
+  else if ('object' !== typeof _)	n.push([p, toJ(_), 'N', typeof _]);
+
+  else if (_.toString)
+    {
+      const t = `${_}`;
+      switch (t)
+        {
+//        case '[object Generator]':	for (const x of _)	yield* dumper(`${p}!`, x, n);
+        default:			n.push([p, _, 'S', _.toString(), typeof _]);
+        }
+    }
+
+//  else for (const k in _())		yield* dumper(`${p}`, _, n);
+  else for (const k in _)		yield* dumper(`${p}.${k}`, _[k], n);
+
+  return n;
+}
 
