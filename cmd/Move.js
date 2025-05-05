8e0220f3f1f12c52225d22992a2a1fd5dd006d20
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-05-05 03:55:25 +0200

    cmd/Move.js modified

--- cmd/Move.js
+++ cmd/Move.js
@@ -1,33 +1,57 @@
 // x,y,z delta: move to location
 // TODO: implement a working pathfinder (mineflayer-pathfinder has issues)
 
-return yield ['TP', _];
+//return yield ['TP', _];
 
+const dest	= _[0];
+const delta	= (_[1]|0)+0.5;
+if (delta >= (yield ['dist', dest])) return ['act already there!'];
 
-// Er hängt im arrive anscheinend (so genau bin ich mir da nicht sicher)
+//console.error('Move', dest);
+let d	= delta;
 
-const act = (...a) => _[2] === null ? void 0 : a;
+function* check(k,l)
+{
+  const p	= yield ['SPOT', l, k];
+  if (!p) return;
 
-const d = yield ['dist', _];
-if (d<2) return act('act already there!');
-
-// XXX TODO XXX !FOLLOWING SHOULD BE REPLACED!
-if (_[2] !== null)
-  yield act('act moving', Math.ceil(d), 'to', _);
-
-try {
-  yield yield ['move', _];
-  yield yield ['arrive'];
-} catch (e) {
-  yield `OOPS: ${e}`;
-  console.error(e);
+  yield yield ['TP', p];
+  return d	= delta;
 }
 
-const t = yield ['dist', _];
-if (t<=1+(_[1]|0))
-  return act('act arrived at',_)
+function* miss(n,d)
+{
+  const p	= yield ['pos'];
+  const v	= p.sub(dest).scale(1/n);		// f(x) := dest + x * (post - dest) / n  --   0..[n-1]
 
-yield act('act distance', ((t*10)|0)/10, 'teleporting to', _);
-yield ['tp', _];
-return ['weit 20'];
+  console.error('miss', p,v);
+  // do a binary search
+
+  let	l	= 0;
+  let h	= n - 1;
+  let x;
+
+  while (l <= h)
+    {
+      const m	= ((n+l) / 2) | 0;
+      const k	= dest.pos(v.scale(m));
+      if (yield* check(k, n))
+        return;
+      l	= m + 1;
+    }
+  return true;
+}
+
+for (;;)
+  {
+    const d	= yield ['dist', dest];
+    if (d <= delta+1) return ['act arrived!', d, delta];
+
+    for (let n=delta || 1; yield* miss(n, d); n *= 2)
+      if (n * 3 > d)
+        {
+          yield ['tp', dest];
+          return ['act sorry no way found', d, n];
+        }
+  }
 
