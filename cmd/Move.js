1f1c3dad743cd3704bfbe31a311cc6b87cb56fec
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-05-09 01:10:34 +0200

    cmd/Move.js modified

--- cmd/Move.js
+++ cmd/Move.js
@@ -15,9 +15,13 @@ function* check(k,l)
   const p	= yield ['SPOT', l, k];
   if (!p) return;
 
+  const opos	= yield ['dist', dest];
+  const npos	= dest.dist(p);
+
+  if (npos >= opos) return;	// yield ['act WTF wrong way', p];
+
   // this moves at least distance 1, hence we have progress
   yield yield ['TP', p];
-  return d	= delta;
 }
 
 function* miss(n,d)
@@ -25,7 +29,14 @@ function* miss(n,d)
   const p	= yield ['pos'];
   const v	= p.sub(dest).scale(1/n);		// f(x) := dest + x * (post - dest) / n  --   0..[n-1]
 
-  console.error('miss', p,v);
+  console.error('V', `${v} ${p} ${dest}`, n, d);
+  console.error('V', `${v} ${p} ${dest}`, n, d);
+  console.error('V', `${v} ${p} ${dest}`, n, d);
+  console.error('V', `${v} ${p} ${dest}`, n, d);
+  console.error('V', `${v} ${p} ${dest}`, n, d);
+  console.error('V', `${v} ${p} ${dest}`, n, d);
+
+  //console.error('miss', p,v);
   // do a binary search
 
   let l	= 0;
@@ -34,9 +45,9 @@ function* miss(n,d)
 
   while (l <= h)
     {
-      const m	= ((n+l) / 2) | 0;
-      const k	= dest.pos(v.scale(m));
-      if (yield* check(k, n))
+      const m	= ((n+l) / 2)|0 || 1;
+      const k	= dest.pos(v.scaled(m));
+      if (yield* check(k, n>30 ? 30 : n))
         return;						// there is progress
       l	= m + 1;
     }
@@ -46,7 +57,7 @@ function* miss(n,d)
 for (;;)
   {
     const d	= yield ['dist', dest];
-    if (d <= delta+1) return ['act arrived!', d, delta];
+    if (d <= delta+1) return ['act arrived', ((d*1000)|0)/1000, 'at', dest];
 
     for (let n=delta || 1; yield* miss(n, d); n *= 2)
       if (n * 3 > d)
