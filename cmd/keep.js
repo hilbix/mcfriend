5960e0edce6e3463ae630aec2f20d0cb51672af7
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-05-11 19:28:30 +0200

    cmd/keep.js modified

--- cmd/keep.js
+++ cmd/keep.js
@@ -6,13 +6,16 @@ const area = yield ['AREA keep'];
 if (area?.length !== 1)
   return yield ['act WTF keep-area', area];
 
-const keep = area.shift();
+const keep	= area.shift();
 
 yield* check_area();
 
 async function* check_area()
 {
-  for await (const b of yield ['block', keep.map(_ => _.pos(0,-1,0))])
-    ;
+  const x = yield ['block', keep.map(_ => _.pos(0,-1,0))];
+  for await (const b of x)
+    {
+      ;
+    }
 }
 
