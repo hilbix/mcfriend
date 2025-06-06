75d85d67f0ce9871383089df07922f3f605e0acb
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-06-06 13:17:26 +0200

    m-data.js added

---
+++ m-data.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env nodejs
+'use strict';
+// This Works is placed under the terms of the Copyright Less License,
+// see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
+
+// Nothing here for now
