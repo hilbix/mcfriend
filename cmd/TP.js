e12b5d5ef038c219071e1e976e2cf8af47e32f9b
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-06-13 03:44:07 +0200

    cmd/TP.js modified

--- cmd/TP.js
+++ cmd/TP.js
@@ -5,13 +5,13 @@ try {
   //yield ['act tp', _, p];
 //  yield yield ['say /gamemode spectator'];
   yield yield ['tp', p];
-  for (let i = 100; --i>=0; await sleep(10))
+  for (let i = 100; --i>=0; yield ['wait', 2])
     {
       const d = yield ['dist', p];
       if (d<4)
         return;
     }
-  
+
   yield ['act teleport to', p, 'failed', yield ['pos']];
 } finally {
 //  yield yield ['say /gamemode survival'];
