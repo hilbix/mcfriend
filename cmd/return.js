25e70a924106e258517ea6348f306999e30ce41d
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-06-14 23:07:49 +0200

    cmd/return.js modified

--- cmd/return.js
+++ cmd/return.js
@@ -1,5 +1,9 @@
 // BOT to BOT communication (Creturn)
 
-const t = _.shift();
-return yield ['say /tellraw', src._, toJ(`return ${t} ${toJ(yield _)}`)];
+const t	= _.shift();
+const s	= toJ(yield _);
+const l	= s.length;
+const k	= 200;
+for (let i=0; i<l; i+=k)
+  yield ['say /tellraw', src._, toJ(`return ${t} ${l} ${i/k} ${s.substr(i,k)}`)];
 
