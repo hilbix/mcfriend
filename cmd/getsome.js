91c0bab8709bf9db4e7893479233f19508167464
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-06-15 01:56:29 +0200

    cmd/getsome.js modified

--- cmd/getsome.js
+++ cmd/getsome.js
@@ -6,11 +6,13 @@ for (const item of _)
   if (yield ['have', item])
     return item;
 
+yield ['home'];
+
 for (const item of _)
   {
     yield [`get ${item.id}=${item.max}`];
     if (yield ['have', item])
-       return item;
+      return item;
   }
 
 for (const item of _)
@@ -18,6 +20,6 @@ for (const item of _)
     yield yield ['CraftItem', item, 1];
     yield ['wait'];
     if (yield ['have', item])
-       return item;
+      return item;
   }
 
