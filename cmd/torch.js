c7fde262c8cd55c846e7a3a8364a5821a4980211
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-10-25 14:23:19 +0200

    cmd/torch.js modified

--- cmd/torch.js
+++ cmd/torch.js
@@ -1,4 +1,7 @@
 // place torches where it is dark
+//
+// torch list
+// torch [item]
 
 this.torched	??= [];
 this.torches	??= 0;
@@ -6,27 +9,29 @@ this.torches	??= 0;
 switch (_[0])
   {
   case 'list':
-    yield ['act', torches, 'torches', torched];
+    return yield ['act', torches, 'torches', torched];
   }
 
-const w = Array.from(yield ['invs']).filter(_ => _.id === 'torch');
-if (!w.length) return yield ['say NO TORCH'];
+const t	= _[0] ?? 'torch';
+
+const w = Array.from(yield ['invs']).filter(_ => _.id === t);
+if (!w.length) return yield ['say NO', t];
 
 const p = yield ['pos'];
 const b = yield ['block', p, 27];
 const pos = [];
 for (const a of b)
   {
-    if (a.id === 'torch') return;
+    if (a.id === t) return;
     if (a.id !== 'air') continue;
-    if (a._.light > 10) return;
+    if (a._.light > 10 && t === 'torch') return;
 
     // check block under torch is something we can place a torch
     const g = yield ['block', a.pos(0,-1,0)];
     if (g.light > 10) return;
     switch (g.id)
       {
-      case 'torch': return;
+      case t: return;
       case 'air':
       case 'water': continue;
       }
@@ -47,8 +52,8 @@ yield ['equip', 'hand', w[0]];
 try {
   yield ['place', pos[0]];
 } catch (e) {
-  return ['note placing torch failed', pos[0]];
+  return ['note placing', t, 'failed', pos[0]];
 }
 
-return ['say placed torch', pos[0]];
+return ['say placed', t, pos[0]];
 
