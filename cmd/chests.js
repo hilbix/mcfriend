795c460aac10f65e9a8603b8f808e668768f17b9
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-11-22 20:21:25 +0100

    cmd/chests.js modified

--- cmd/chests.js
+++ cmd/chests.js
@@ -3,15 +3,23 @@
 const signs = yield ['sign', _];
 if (!signs) return;
 
+let ok=0, bad=0, oth=0;
+
 for (const s of signs)
   {
 //    if (!s.valid) continue;
-    yield ['act sign', s, yield ['locate', s]];
+//    yield ['act sign', s, yield ['locate', s]];
 
     const d = (yield ['block', s, 6]).filter(_ => _.container);
     if (d.length === 1)
-      yield ['act sign', s, 'at', yield ['locate', s], s.valid];
+      ok++;
+    else if (d.length === 0)
+      oth++;
     else
-      yield ['act invalid:', s, s.valid];
+      {
+        yield ['act invalid:', s, s.valid];
+	bad++;
+      }
   }
 
+yield ['act signs chest', ok, 'other', oth, 'bad', bad];
