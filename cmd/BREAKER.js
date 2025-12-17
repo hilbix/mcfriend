004d102317b308b653b427a8082d138799bac963
Valentin Hilbig <webmaster@scylla-charybdis.com>
2025-12-17 13:31:46 +0100

    cmd/BREAKER.js modified

--- cmd/BREAKER.js
+++ cmd/BREAKER.js
@@ -4,9 +4,7 @@
 
 for (const b of _)
   {
-    const p	= yield ['SPOT', 5, b];
-    if (b)
-       yield ['Move', b];
+    yield ['Move', b];
     const {x,y,z} = b.vec();
     yield ['say /execute run fill',x,y,z,x,y,z,'air destroy'];
     yield yield ['wait'];
