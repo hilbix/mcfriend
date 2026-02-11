c0e11844b41e5d1e92d5d32dd6253d51a1f3b407
Valentin Hilbig <webmaster@scylla-charybdis.com>
2026-02-11 21:57:48 +0100

    cmd/DIGGER.js modified

--- cmd/DIGGER.js
+++ cmd/DIGGER.js
@@ -6,23 +6,34 @@ const	q = _.shift(_);
 
 const items= yield ['item', _];
 if (!items || !items.length) throw `WTF? no items ${_}`;
-
 const want = Object.fromEntries(items.map(_ => [_.id,true]));
+
+let it = [];
+try {
+  it	= yield ['item', _.map(_ => `_${_}`)];
+} catch (e) {
+}
+const ign = Object.fromEntries(it.map(_ => [_.id,true]));
 
 let did = false;
 let n=50;
 let m=1000;
 while (--m>=0 && n>=0 && (next = yield* q()))
   {
-//    yield ['act DIGGER', n, next];
     if (want[next.id])
       {
         yield ['BREAKER', next];
         n--;
         did = true;
+        continue;
       }
-    else
-      yield* q.ok(next);
+    yield* q.ok(next);
+
+    if (!ign[next.id])
+      {
+	ign[next.id] = 1;
+        yield ['report DIGGER', next, _];
+      }
   }
 
 q.end();
