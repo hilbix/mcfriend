ae3bdfb263d9975c2f68c91fcf3a38a6a5b25b11
Valentin Hilbig <webmaster@scylla-charybdis.com>
2026-04-23 21:53:38 +0200

    cmd/PLACE.js modified

--- cmd/PLACE.js
+++ cmd/PLACE.js
@@ -1,13 +1,13 @@
 // Place a block
 //
-// This (ab)uses low level Mineflayer internals to do it right
+// This (ab)uses low level Mineflayer internals to do it right.
 //
-// yield ['PLACE', item, dest, ref, orient]
+// yield ['PLACE', item, dest, ref]
 //
 // item=	item name, will be fetched if not in inv
-// dest=	position to put
-// ref=		block to click on to place (default: block below dest)
-// orient=	orientation (default: from ref)
+// dest=	position where to put item
+// ref=		reference (first letter, lowercase)
+//		either block or North West East South Up Down (default: d)
 //
 // More options possibly follow
 
@@ -23,13 +23,13 @@ function ev(ev, check, timeout)
   const on = (..._) =>
     {
       try {
-        console.error('ON', _);
+//        console.error('ON', _);
         const r = check(..._);
-        console.error('ON:', r);
+//        console.error('ON:', r);
         if (r) x.o(r);
       } catch (e) {
         console.error(e);
-	x.k(e);
+        x.k(e);
       }
     };
 
@@ -43,49 +43,62 @@ function ev(ev, check, timeout)
 
 // Stolen from mineflayer/lib/plugins/place_block.js
 // dest		Block to change
-// ref		Block to put things on
-// ori		Block/Pos etc. or Vec3 for facing
-async function* place_block(dest, ref, ori)
+// ref		reference point
+async function* place_block(dest, ref, p)
 {
   const b	= yield ['block', dest];
-  const r	= yield ['block', ref];
-  const delta	= b.sub(r);
-  delta.x	= 0.5 + delta.x * 0.5;
-  delta.y	= 0.5 + delta.y * 0.5;
-  delta.z	= 0.5 + delta.z * 0.5;
-  if (delta.y === 0.5) delta.y = 0.25;	// lower half
-  yield ['act placing', b, 'onto', r, 'facing', ori, 'with', delta];
+  const r	= yield ['block', b.pos(ref)];
+  const d	= b.sub(r);
 
+  yield ['act placing', b, 'onto', r, 'with', ref._vec];
+
+  yield ['TP', p.pos(0,b.sub(p).y,0)];
   const e	= ev(`blockUpdate:${b._vec}`, (o,n) => { if (!o || !n || o.type !== n.type) return [o,n] }, 5000);
-  await B._genericPlace(r._, ori, { delta, swingArm:'right' });
-  const [o, n] = await e;
-
-  yield ['act have', o?.type, n?.type, o?.location, n?.location];
-  if (!o && !n) return;
-  if (o?.type === n?.type) {
-    throw new Error(`No block has been placed : the block is still ${o?.name}`)
-  } else {
-    B.emit('blockPlaced', o, n)
+  await B._genericPlace(r._, d, { swingArm:'right' });
+  yield ['TP', p];
+  try {
+    const [o, n] = await e;
+    yield ['act have', o?.type, n?.type, o?.location, n?.location];
+    if (!o && !n) return;
+    if (o?.type === n?.type) {
+      throw new Error(`No block has been placed : the block is still ${o?.name}`)
+    } else {
+      B.emit('blockPlaced', o, n)
+    }
+  } finally {
   }
+}
+
+const DIR =
+  { w: [-1,  0,  0]
+  , e: [ 1,  0,  0]
+  , d: [ 0, -1,  0]
+  , u: [ 0,  1,  0]
+  , n: [ 0,  0, -1]
+  , s: [ 0,  0,  1]
+  };
+
+function dir(r, b)
+{
+  if (isMy(r))
+    return r.sub(b);
+  return DIR[_.shift()] ?? DIR.d;
 }
 
 const B		= __ABI__.B;
 const item	= yield ['item', _.shift()];
-const dest	= yield ['block', _.shift() ]
-const ref	= yield ['block', _.shift() ?? dest.pos(0,-1,0) ]
-const ori	= _.shift() ?? ref;
-//const opt	= _.reduce((a,_) => Object.entries(_).reduce((a,[k,v]) => { a[k]=v; return a }, a), {});
+const dest	= yield ['block', _.shift() ];
+const ref	= yield ['pos', dir(_.shift(), dest)];
 
 const p		= yield ['pos'];
 const i		= yield ['getsome', item];
-yield ['TP', p];
 
 yield ['equip hand', i.type];
 await B.setControlState('sneak', true);
-yield ['wait', 10];
+//yield ['wait', 10];
 
-yield* place_block(dest, ref, isMy(ori) ? ori.sub(dest) : ori);
+yield* place_block(dest, ref, p);
 
 await B.setControlState('sneak', false);
-yield ['wait', 10];
+//yield ['wait', 10];
 
