4a5cf71ae5ce3d21084e911ccf4a4213798c6cf4
Valentin Hilbig <webmaster@scylla-charybdis.com>
2026-06-03 22:05:17 +0200

    cmd/CHEST.js modified

--- cmd/CHEST.js
+++ cmd/CHEST.js
@@ -9,13 +9,20 @@ for (const s_ of signs)
   {
     const s = yield ['validsign', s_];
     if (!s) continue;
-//    yield ['act sign', s, yield ['locate', s]];
 
-    const d = (yield ['block', s, 6]).filter(_ => _.container);
+    //yield ['act sign', s, yield ['locate', s]];
+
+    const t = yield ['SPOT', 5, s];
+    if (t !== false)
+      yield ['tp', t ?? s];
+    yield ['wait', 3];
+
+    const bs = yield ['block', s, 6];
+    const d = bs.filter(_ => _.container);
     if (d.length === 1)
       r.push([d[0],s]);
     else
-      yield ['note Invalid', s, s.valid];
+      yield ['note Invalid', d.length, s, s.valid, bs];
   }
 return r;
 
