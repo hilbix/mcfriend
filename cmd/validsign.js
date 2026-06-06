216d2e760e0453a5a4d28fda7da4c19667f1759a
Valentin Hilbig <webmaster@scylla-charybdis.com>
2026-06-06 19:11:47 +0200

    cmd/validsign.js modified

--- cmd/validsign.js
+++ cmd/validsign.js
@@ -8,16 +8,26 @@ if (validSign(_[0]))
   return _[0];
 
 const s	= _[0];
-if (s === 'CLR') this.invalid = {};
+if (s === 'CLR')
+  {
+    const r	= this.invalid;
+    this.invalid = {};
+    return r;
+  }
 const known = invalid[`${s}`];
+
+if (known === true) return;	// sign known to not be valid
 
 if (!known)
   yield ['act VS checking', s];
 
 const t = yield ['SPOT', 5, s];
 if (t !== false)
-  yield ['tp', t ?? s];
-yield ['wait', 30];
+  {
+    yield ['tp', t ?? s];
+    BUG('VS wait', t, s);
+    yield ['wait', 30];
+  }
 
 const b = yield ['sign', s];
 if (!b.valid)
@@ -25,6 +35,7 @@ if (!b.valid)
     if (!known)
       yield ['act', s, 'not valid'];
     invalid[`${s}`] = true;
+    BUG('VS invalid', s);
     return; // 'sign is not valid';
   }
 
@@ -33,6 +44,7 @@ if (!b.valid)
 //if (x !== y)
 //  throw `sign has changed!?! ${x} != ${y}`;
 
+BUG('VS valid', s);
 
 return b;
 
